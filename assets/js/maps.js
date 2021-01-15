9e3ece47072af801d48da4cbfd37fabcf34f7475
daithioc95 <daithioc@outlook.com>
2021-01-15 21:21:10 +0000

    edited maps.js to show div when marker clicked

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -121,32 +121,33 @@ document.querySelector("input[name=eventsRadio]").addEventListener('change', fun
 
 
 var attractions = [
-    [52.9717877, -9.4291244, '<h1>Cliffs of Moher</h1>'], 
-    [53.34193275, -6.28672974298643, '<h1>Guiness Storehouse</h1>'], 
-    [55.24082145, -6.51163864, '<h1>Giants Causeway</h1>'],
-    [53.34110865, -6.25267, '<h1>National Gallery</h1>'],
-    [53.3606275 , -6.25263, '<h1>Croke Park</h1>'],
-    [53.0112891 , -6.32609, '<h1>Glendalough</h1>']
+    [52.9717877, -9.4291244, '<button id onclick="showelement()">Cliffs of Moher</button>'], 
+    [53.34193275, -6.28672974298643, '<h6>Guiness Storehouse</h6>'], 
+    [55.24082145, -6.51163864, '<h6>Giants Causeway</h6>'],
+    [53.34110865, -6.25267, '<h6>National Gallery</h6>'],
+    [53.3606275 , -6.25263, '<h6>Croke Park</h6>'],
+    [53.0112891 , -6.32609, '<h6>Glendalough</h6>']
 ];
 
 var restaurants = [
-    [53.3454622, -6.26411, '<h1>The Temple Bar</h1>'], 
-    [53.335436, -6.26351, '<h1>Coppers</h1>'], 
-    [53.33815205, -6.25418, '<h1>ODonoghues</h1>'],
-    [54.6021395, -5.92705, '<h1>Harp Bar</h1>'],
-    [51.9017201, -8.47109, '<h1>Sin é</h1>']
+    [53.3454622, -6.26411, '<h6>The Temple Bar</h6>'], 
+    [53.335436, -6.26351, '<h6>Coppers</h6>'], 
+    [53.33815205, -6.25418, '<h6>ODonoghues</h6>'],
+    [54.6021395, -5.92705, '<h6>Harp Bar</h6>'],
+    [51.9017201, -8.47109, '<h6>Sin é</h6>']
 ];
 
 var events = [
-    [53.69469735, -6.47555 , '<h1>Newgrange</h1>'],
-    [53.3528582, -6.2645, '<h1>St. Patricks Day Festival</h1>'], 
-    [52.33819075, -6.46211, '<h1>Wexford Opera Festival</h1>'],
-    [52.0595746, -9.50532, '<h1>Ring of Kerry cycle</h1>'],
-    [53.62569255, -6.88866, '<h1>Púca Halloween Festival</h1>']
+    [53.69469735, -6.47555 , '<h6>Newgrange</h6>'],
+    [53.3528582, -6.2645, '<h6>St. Patricks Day Festival</h6>'], 
+    [52.33819075, -6.46211, '<h6>Wexford Opera Festival</h6>'],
+    [52.0595746, -9.50532, '<h6>Ring of Kerry cycle</h6>'],
+    [53.62569255, -6.88866, '<h6>Púca Halloween Festival</h6>']
 ];
 
 
 var marker, i;
+var infowindow = new google.maps.InfoWindow();
 const attractionIcon = "http://maps.google.com/mapfiles/ms/icons/green.png";
 const restaurantIcon = "http://maps.google.com/mapfiles/ms/icons/grey.png";
 const eventIcon = "http://maps.google.com/mapfiles/ms/icons/orange.png";
@@ -165,6 +166,8 @@ const eventIcon = "http://maps.google.com/mapfiles/ms/icons/orange.png";
             return function() {
                 smoothZoom(map, 12, map.getZoom());
                 map.setCenter(marker.getPosition());
+                infowindow.setContent(attractions[i][2]);
+                infowindow.open(map, marker);
             }
         })(marker, i));
     //                                 // Option3
@@ -205,6 +208,8 @@ const eventIcon = "http://maps.google.com/mapfiles/ms/icons/orange.png";
             return function() {
                 smoothZoom(map, 12, map.getZoom());
                 map.setCenter(marker.getPosition());
+                infowindow.setContent(restaurants[i][2]);
+                infowindow.open(map, marker);
             }
         })(marker, i));
     //                         // Option3
@@ -251,30 +256,6 @@ const eventIcon = "http://maps.google.com/mapfiles/ms/icons/orange.png";
         })(marker, i));
 }
 }
-    //                 // Option3
-    //     for (i = 0; i < eventsMarkers.length; i++) {
-    //     marker = new google.maps.Marker({
-    //         position: new google.maps.LatLng(eventsMarkers[i][0], eventsMarkers[i][1]),
-    //         map: map,
-    //         icon: eventIcon
-    //     });
-    // }
-    //     // Option2
-    //   marker.addListener('click', function() {
-    //   map.setZoom(8);
-    //   map.setCenter(marker.getPosition());
-    //   });
- 
-//  Option1
-    //   google.maps.event.addListener(marker, 'click', (function(marker, i) {
-    //     return function() {
-    //     //   infowindow.setContent(events[i][0]);
-    //     //   infowindow.open(map, marker);
-    //     map.setZoom(8);
-    //     map.setCenter(marker.getPosition());
-    //     }
-    //   })(marker, i));
-    
 
     function removeEvents(){
         for(i=0; i<eventsMarkers.length; i++){
@@ -294,5 +275,19 @@ function smoothZoom (map, max, cnt) {
         setTimeout(function(){map.setZoom(cnt)}, 80); // 80ms is what I found to work well on my system -- it might not work well on all systems
     }
 }
+
+}
+
+function showelement(){
+//  var x = document.getElementById("attraction1");
+//   if (x[0].style.display === "none" && x[1].style.display === "none") {
+//     x[0].style.display = "block";
+//     x[1].style.display = "block";
+//   } else {
+//     x[0].style.display = "none";
+//     x[1].style.display = "none"
+//   }
+$("#attraction1").toggle();
+$(".location-image").toggle();
 }
 // next steps : event handlers for markers clicks, eg. zoom, prompt html etc.
