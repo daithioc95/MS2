bae7db8098c103840e911a2d62f95955585b352c
daithioc95 <daithioc@outlook.com>
2021-02-04 19:46:13 +0000

    updated calendar.js to improve comments and formatting

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,81 +1,29 @@
-// Current date is 25th March 2020
+// Evo-calendar source: https://github.com/edlynvillegas/evo-calendar
+// Starting dates for each tour category
 var fmDate = new Date("2021-02-12");
 var fnDate = new Date("2021-02-12");
 var SmDate = new Date("2021-02-13");
 var SnDate = new Date("2021-02-13");
 var events = [];
 
-for (var i=0; i<30; i++) {
-  var dd = fmDate.getDate();
-  var mm = fmDate.getMonth()+1; 
-  var yyyy = fmDate.getFullYear();
+// for loop to add friday morning event for specified dates
+// Source: https://www.foxinfotech.in/2020/03/javascript-increment-date-by-1-day-examples.html
+for (var i = 0; i < 30; i++) {
+    var dd = fmDate.getDate();
+    var mm = fmDate.getMonth() + 1;
+    var yyyy = fmDate.getFullYear();
 
-  if(dd<10) 
-  {
-      dd='0'+dd;
-  } 
-  if(mm<10) 
-  {
-      mm='0'+mm;
-  } 
-
- var date = mm+'/'+dd+'/'+yyyy; 
- fmDate.setDate(fmDate.getDate() + 7);  
+    if (dd < 10) {
+        dd = '0' + dd;
+    }
+    if (mm < 10) {
+        mm = '0' + mm;
+    }
+    // assign event with relevant info
+    var date = mm + '/' + dd + '/' + yyyy;
+    fmDate.setDate(fmDate.getDate() + 7);
     events[i] = {
-        id: 'Friday Morning Tour' + (i+1) , // Event's ID (required)
-        name: "Morning Tour 9.00",// Event name (required)
-        badge: "Book Now", // Event badge (optional)
-        date: (date), // Event date (required)
-        type: "event",  // Event type (required)
-        color: "yellow"
-    };
-}
-// https://www.foxinfotech.in/2020/03/javascript-increment-date-by-1-day-examples.html
-
-for (var i=30; i<60; i++) {
-  var dd = fnDate.getDate();
-  var mm = fnDate.getMonth()+1; 
-  var yyyy = fnDate.getFullYear();
-
-  if(dd<10) 
-  {
-      dd='0'+dd;
-  } 
-  if(mm<10) 
-  {
-      mm='0'+mm;
-  } 
-
- var date = mm+'/'+dd+'/'+yyyy; 
- fnDate.setDate(fnDate.getDate() + 7);  
-    events[i] = {
-        id: 'Friday Noon Tour' + (i+1) , // Event's ID (required)
-        name: "Noon Tour 12.00",// Event name (required)
-        badge: "Book Now", // Event badge (optional)
-        date: (date), // Event date (required)
-        type: "holiday",  // Event type (required)
-        color: "red"
-    };
-}
-
-for (var i=60; i<90; i++) {
-  var dd = SmDate.getDate();
-  var mm = SmDate.getMonth()+1; 
-  var yyyy = SmDate.getFullYear();
-
-  if(dd<10) 
-  {
-      dd='0'+dd;
-  } 
-  if(mm<10) 
-  {
-      mm='0'+mm;
-  } 
-
- var date = mm+'/'+dd+'/'+yyyy; 
- SmDate.setDate(SmDate.getDate() + 7);  
-    events[i] = {
-        id: 'Saturday Morning Tour' + (i+1) , // Event's ID (required)
+        id: 'Friday Morning Tour' + (i + 1), // Event's ID (required)
         name: "Morning Tour 9.00",// Event name (required)
         badge: "Book Now", // Event badge (optional)
         date: (date), // Event date (required)
@@ -84,24 +32,23 @@ for (var i=60; i<90; i++) {
     };
 }
 
-for (var i=90; i<120; i++) {
-  var dd = SnDate.getDate();
-  var mm = SnDate.getMonth()+1; 
-  var yyyy = SnDate.getFullYear();
+// for loop to add friday noon event for specified dates
+for (var i = 30; i < 60; i++) {
+    var dd = fnDate.getDate();
+    var mm = fnDate.getMonth() + 1;
+    var yyyy = fnDate.getFullYear();
 
-  if(dd<10) 
-  {
-      dd='0'+dd;
-  } 
-  if(mm<10) 
-  {
-      mm='0'+mm;
-  } 
-
- var date = mm+'/'+dd+'/'+yyyy; 
- SnDate.setDate(SnDate.getDate() + 7);  
+    if (dd < 10) {
+        dd = '0' + dd;
+    }
+    if (mm < 10) {
+        mm = '0' + mm;
+    }
+    // assign event with relevant info
+    var date = mm + '/' + dd + '/' + yyyy;
+    fnDate.setDate(fnDate.getDate() + 7);
     events[i] = {
-        id: 'Saturday Noon Tour' + (i+1) , // Event's ID (required)
+        id: 'Friday Noon Tour' + (i + 1), // Event's ID (required)
         name: "Noon Tour 12.00",// Event name (required)
         badge: "Book Now", // Event badge (optional)
         date: (date), // Event date (required)
@@ -110,6 +57,57 @@ for (var i=90; i<120; i++) {
     };
 }
 
+// for loop to add saturday morning event for specified dates
+for (var i = 60; i < 90; i++) {
+    var dd = SmDate.getDate();
+    var mm = SmDate.getMonth() + 1;
+    var yyyy = SmDate.getFullYear();
+
+    if (dd < 10) {
+        dd = '0' + dd;
+    }
+    if (mm < 10) {
+        mm = '0' + mm;
+    }
+    // assign event with relevant info
+    var date = mm + '/' + dd + '/' + yyyy;
+    SmDate.setDate(SmDate.getDate() + 7);
+    events[i] = {
+        id: 'Saturday Morning Tour' + (i + 1), // Event's ID (required)
+        name: "Morning Tour 9.00",// Event name (required)
+        badge: "Book Now", // Event badge (optional)
+        date: (date), // Event date (required)
+        type: "event",  // Event type (required)
+        color: "yellow"
+    };
+}
+
+// for loop to add saturday evening event for specified dates
+for (var i = 90; i < 120; i++) {
+    var dd = SnDate.getDate();
+    var mm = SnDate.getMonth() + 1;
+    var yyyy = SnDate.getFullYear();
+
+    if (dd < 10) {
+        dd = '0' + dd;
+    }
+    if (mm < 10) {
+        mm = '0' + mm;
+    }
+    // assign event with relevant info
+    var date = mm + '/' + dd + '/' + yyyy;
+    SnDate.setDate(SnDate.getDate() + 7);
+    events[i] = {
+        id: 'Saturday Noon Tour' + (i + 1), // Event's ID (required)
+        name: "Noon Tour 12.00",// Event name (required)
+        badge: "Book Now", // Event badge (optional)
+        date: (date), // Event date (required)
+        type: "holiday",  // Event type (required)
+        color: "red"
+    };
+}
+
+// evoCalendar functions source: source: https://github.com/edlynvillegas/evo-calendar
 // initialize evo-calendar
 $(document).ready(function () {
     $('#calendar').evoCalendar({
@@ -118,18 +116,18 @@ $(document).ready(function () {
     })
 })
 
-$('#calendar').on('selectEvent', function(event, activeEvent) {
-     $('#exampleModalCenter').modal('toggle');
+// Open booking form on event selection
+$('#calendar').on('selectEvent', function (event, activeEvent) {
+    $('#exampleModalCenter').modal('toggle');
 });
 
-// https://www.tutorialrepublic.com/faq/how-to-detect-a-mobile-device-in-jquery.php
-$(document).ready(function(){
-    if(window.matchMedia("(max-width: 425px)").matches){
-        // The viewport is less than 768 pixels wide
-        $('#calendar').on('selectDate', function(event, newDate, oldDate) {
-         $('#calendar').evoCalendar('toggleEventList', true);
-   });
-    } else{
-        // The viewport is at least 768 pixels wide
+// Event listings toggle open automatically on date selection for mobile
+// Source for media size detection: https://www.tutorialrepublic.com/faq/how-to-detect-a-mobile-device-in-jquery.php
+$(document).ready(function () {
+    if (window.matchMedia("(max-width: 425px)").matches) {
+        $('#calendar').on('selectDate', function (event, newDate, oldDate) {
+            $('#calendar').evoCalendar('toggleEventList', true);
+        });
+    } else {
     }
 });
